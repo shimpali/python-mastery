3148ba48f0fee2edaf4f32fed940bbfd3eb57a7c
shimpali <shimpali.deshpande@gmail.com>
2023-07-31 17:41:09 +0530

    1_3 + Reformat with PyCharm

---
+++ Exercises/1_3/pcost.py
@@ -0,0 +1,15 @@
+"""
+>>> calculate_total_cost()
+44671.15
+"""
+
+
+def calculate_total_cost():
+    with open('/Users/shimpali/PycharmProjects/python-mastery/Data/portfolio.dat', 'r') as file:
+        cost = 0.0
+        for line in file:
+            columns = line.split()
+            no_of_shares = int(columns[1])
+            purchase_price = float(columns[2])
+            cost += no_of_shares * purchase_price
+        return cost
