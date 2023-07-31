f710de12e8d5a2ff39de5fec38c5b14a7b8eb4dd
shimpali <shimpali.deshpande@gmail.com>
2023-07-31 18:35:06 +0530

    1_6

---
+++ Exercises/defining_modules.py
@@ -0,0 +1,15 @@
+"""
+Exercise: 1_6
+
+>>> import pcost
+44671.15
+>>> pcost.portfolio_cost('Data/portfolio2.dat')
+19908.75
+>>> from stock import Stock
+>>> s = Stock('GOOG', 100, 490.10)
+>>> s.name
+'GOOG'
+>>> s.cost()
+49010.0
+>>>
+"""
